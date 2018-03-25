1086b09489f4fec21d59558a3a68cf8067c90d73
itaouil <itaouil95@gmail.com>
2018-03-25 11:47:50 +0100

    Fixed logic mistake.

--- modules/ann.py
+++ modules/ann.py
@@ -11,23 +11,23 @@ from preprocessing import *
 classifier = Sequential()
 
 # Input layer
-classifier.add(Dense(output_dim=6, init="uniform", activation="relu", input_dim=11))
+classifier.add(Dense(activation="relu", input_dim=11, units=6, kernel_initializer="uniform"))
 
 # Hidden layer
-classifier.add(Dense(output_dim=6, init="uniform", activation="relu"))
+classifier.add(Dense(activation="relu", units=6, kernel_initializer="uniform"))
 
 # Output layer
-classifier.add(Dense(output_dim=1, init="uniform", activation="sigmoid"))
+classifier.add(Dense(activation="sigmoid", units=1, kernel_initializer="uniform"))
 
-# Apply stochastic
-# gradient descent
+# Apply stochastic gradient descent
 classifier.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
 
 # Fit ANN to training set
-classifier.fit(x_train, y_train, batch_size=10, nb_epoch=100)
+classifier.fit(x_train, y_train, batch_size=10, epochs=100)
 
 # Make predictions & Confusion matrix
 y_pred = classifier.predict(x_test)
 y_pred = (y_pred > 0.5)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+print(cm)
--- modules/preprocessing.py
+++ modules/preprocessing.py
@@ -25,10 +25,10 @@ x = x[:, 1:]
 
 # Split dataset into training and test sets
 from sklearn.model_selection import train_test_split
-x_train, y_train, x_test, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
 # Feature scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 x_train = sc.fit_transform(x_train)
-x_test  = sc.transform(x_test.reshape(-1, 1))
+x_test  = sc.transform(x_test)
